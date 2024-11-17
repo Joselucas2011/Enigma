28fa60117bea278611aee83a22049781eccab2d2
Joselucas2011 <joseeritinha2024@gmail.com>
2024-11-17 12:12:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    const password = document.getElementById("password").value;
+    
+    // Verifica se a senha está correta
+    if (password === "kkkk") {
+        // Redireciona para outro arquivo HTML
+        window.location.href = "pao.html";
+    } else {
+        // Alerta caso a senha esteja errada
+        alert("Senha incorreta! Tente novamente.");
+    }
+    document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    const password = document.getElementById("password").value;
+    
+    // Verifica se a senha está correta
+    if (password === "kkkk") {
+        // Redireciona para outro arquivo HTML
+        window.location.href = "pagina-secreta.html";
+    } else {
+        // Alerta caso a senha esteja errada
+        alert("Senha incorreta! Tente novamente.");
+    }
+});
+
+
+});
+
